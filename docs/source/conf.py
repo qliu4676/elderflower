a5fc73bb0312fdb2f87af71ddd67aed92763b8d5
Qing Liu <qliu@astro.utoronto.ca>
2020-11-02 17:57:05 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
 
+autodoc_mock_imports = ['galsim']
+
 # -- Project information -----------------------------------------------------
 
 project = 'elderflower'
